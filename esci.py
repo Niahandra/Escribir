c4dc3258f022315834e54758e15396b574e96c9b
Niahandra <niahandrajeanbaptiste@gmail.com>
2026-02-20 13:24:38 -0400

    e

--- esci.py
+++ esci.py
@@ -10,5 +10,14 @@ def main(page: ft.Page):
     random.shuffle(words)
     game_words = words[:17]
 
+    mistakes = 0
+    current_index = 0
+
+    word_label = ft.Text(value=f"Word: {game_words[current_index]}", size=20)
+    status_label = ft.Text(value="", size=19)
+    progress_label = ft.Text(value=f"Progress: {current_index}/{len(game_words)}", size=16)
+    accuracy_label = ft.Text(value="Accuracy: 0%", size=14)
+    input_field = ft.TextField(hint_text="Type here", autofocus=True, width=270)
+
 
 ft.app(target=main)
