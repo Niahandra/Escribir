1e68b7dacb7d0da2df7e544f81d79a7773634660
Niahandra <niahandrajeanbaptiste@gmail.com>
2026-02-20 13:20:03 -0400

    c

--- esci.py
+++ esci.py
@@ -2,5 +2,10 @@ import flet as ft
 import random
 
 def main(page: ft.Page):
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+
+
 
 ft.app(target=main)
